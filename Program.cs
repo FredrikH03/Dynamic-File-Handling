bc8a1d74f454b79ed116609b28c8e928835cf9a3
FredrikH03 <fredrikholst03@hotmail.com>
2023-10-09 16:08:53 +0200

    Initial Commit
    attempt att making dynamic file reading and creating based of a number
    chosen by a user

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+string userinput = Console.ReadLine();
+
+int usernumber = int.Parse(userinput);
+
+Console.WriteLine("user test{0}", usernumber);
+
+string[] file = File.ReadAllLines("~/source/repos/Dynamic-File-Creation/files/test{0}.txt", usernumber);
+
+Console.WriteLine(file);
