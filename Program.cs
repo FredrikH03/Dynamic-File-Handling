d0ae936fcec9e0eb4988b4d65366c563a8027698
FredrikH03 <fredrikholst03@hotmail.com>
2023-10-25 09:57:35 +0200

    added a create directory with string manipulation from manuel

--- Program.cs
+++ Program.cs
@@ -11,4 +11,13 @@ string[] file = File.ReadAllLines($"../../../test{usernumber}.txt");
 foreach (string line in file)
 {
     Console.WriteLine(line);
-}    
+}
+
+
+string path = @"receipts/";
+Directory.CreateDirectory(path);
+string[] files = Directory.GetFiles(path);
+foreach (string file1 in files)
+{
+    Console.WriteLine(file1.Split(path)[1].Split(".csv")[0]);
+}
